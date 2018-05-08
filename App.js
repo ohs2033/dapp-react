8406cc6c5b69afa320b3e54a95f65e1050267ad0
hs.o <ohs@ab180.co>
2018-05-09 02:39:29 +0900

    add form and submit button and pickwinner button

--- App.js
+++ App.js
@@ -6,14 +6,63 @@ import lottery from './lottery';
 
 class App extends Component {
   state = {
-    manager: ''
+    manager: '',
+    players: [],
+    balance: '',
+    value: '',
+    message: 'do you want to enter?'
   }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: 'Wating on transaction success...'})
+    
+    try {
+      /*
+      this code will take some time...about 15~30 seconds.
+       */
+      await lottery.methods.enter().send({ 
+        from: accounts[0],
+        value: web3.utils.toWei(this.state.value, 'ether')
+      });  
+    } catch (err) {
+      window.alert(err);
+      this.setState({
+        message: 'fail!'
+      })
+      return;
+    }
+    this.setState({
+      message: 'You have been entered!'
+    })
+  }
+
+  onClick = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({
+      message: 'Wating on transaction success....'
+    });
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    this.setState({ message: 'A winner has benn picked!'})
+  }; 
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call(
       /* from: accounts[0] */);
-    this.setState({ manager });
+    const players = await lottery.methods.getPlayers.call();;
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager , players, balance});
   }
+
+
 
   render() {
     console.log(web3.version);
@@ -21,7 +70,33 @@ class App extends Component {
     return (
       <div className="App">
         <h2> Lottery Contract</h2>
-        <p> This contract is managed by {this.state.manager}</p>
+        <p> This contract is managed by {this.state.manager}
+          There are currently {this.state.players.length} people.
+          competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+        </p>
+        <hr />
+
+        <form>
+          <h4>Want to try your luck?</h4>
+          <div>
+          <input 
+            value={this.state.value}
+            onChange={event => this.setState({ value: event.target.value })}
+          />
+          <button
+            onClick={this.onSubmit}
+          > Enter </button>
+
+          </div>
+        </form>
+        <hr />
+        <h1>{this.state.message}</h1>
+
+        <hr />
+        <h4> Ready to pick a winner</h4>
+        <button onClick={this.onClick}> Pick a winner!</button>
+
+
       </div>
     );
   }
