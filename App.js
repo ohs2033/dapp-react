ff62c3f6a5ad2a87d690b7abe5bd4435367c3745
hs.o <ohs@ab180.co>
2018-05-09 02:11:09 +0900

    initial commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import web3 from './web3';
+import lottery from './lottery';
+
+class App extends Component {
+  state = {
+    manager: ''
+  }
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call(
+      /* from: accounts[0] */);
+    this.setState({ manager });
+  }
+
+  render() {
+    console.log(web3.version);
+    web3.eth.getAccounts().then(console.log)
+    return (
+      <div className="App">
+        <h2> Lottery Contract</h2>
+        <p> This contract is managed by {this.state.manager}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ lottery.js
@@ -0,0 +1,111 @@
+import web3 from './web3';
+const address = '0xf1240be6aA8dC6F29fCF03d069451a7f3a613CA3';
+
+/*
+아래 카피가 어떤것을 의미하는 것은 아니다.
+단지 그냥 interface를 그대로 객체로 가져와서 넣은 것 뿐.
+local mock이라고 보면 될듯.
+ */
+const abi = [
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "manager",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "pickWinner",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "random",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getPlayerLength",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getPlayers",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address[]"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "enter",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "players",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  }
+];
+
+export default new web3.eth.Contract(abi, address);
